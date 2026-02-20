bc6ca42107df687f581de64ac2ff6fc9befed764
Anya Jha <anyamrendrajha@gmail.com>
2026-02-20 12:55:18 +0530

    Create Hero.js

---
+++ frontend/src/components/Hero.js
@@ -0,0 +1,22 @@
+import "../styles/hero.css";
+
+export default function Hero() {
+
+  return (
+<div className="hero">
+<h1>Freshly Baked Cakes</h1>
+<p>45-Minute Delivery • Made with Love</p>
+
+
+<p>Handcrafted every morning with the finest ingredients, 
+delivered warm and fresh to your doorstep 
+— because every celebration deserves perfection.
+ </p>
+ 
+
+</div>
+
+  );
+
+}
+ 
