9ab9697f1170a5a932605e5da875f652d9795116
Anya Jha <anyamrendrajha@gmail.com>
2026-02-20 12:52:49 +0530

    Create AdminRoutes.js

---
+++ frontend/src/components/AdminRoutes.js
@@ -0,0 +1,20 @@
+import { Navigate } from "react-router-dom";
+
+import { useAuth } from "../context/AuthContext";
+
+export default function AdminRoute({ children }) {
+
+  const { user } = useAuth();
+
+  if (!user) return <Navigate to="/login" />;
+
+  if (user.role !== "vendor" && user.role !== "admin") {
+
+    return <Navigate to="/home" />;
+
+  }
+
+  return children;
+
+}
+ 
