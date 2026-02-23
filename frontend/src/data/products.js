371aa86e6c629e52610f67f879e3513944c10ae3
Anya Jha <anyamrendrajha@gmail.com>
2026-02-23 10:10:51 +0530

    Create products.js

---
+++ frontend/src/data/products.js
@@ -0,0 +1,68 @@
+export const products = [
+
+  {
+
+    id: 1,
+
+    name: "Red Velvet Cake",
+
+    price: 699,
+
+    img: "/images/redvelvet.jpg",
+
+    desc: "Rich red velvet cake",
+
+    category: "birthday"
+
+  },
+
+  {
+
+    id: 2,
+
+    name: "Wedding Special Cake",
+
+    price: 1999,
+
+    img: "/images/wedding.jpg",
+
+    desc: "Elegant wedding cake",
+
+    category: "wedding"
+
+  },
+
+  {
+
+    id: 3,
+
+    name: "Plum Festive Cake",
+
+    price: 899,
+
+    img: "/images/plum.jpg",
+
+    desc: "Perfect festive treat",
+
+    category: "festive"
+
+  },
+
+  {
+
+    id: 4,
+
+    name: "Classic Fruit Cake",
+
+    price: 599,
+
+    img: "/images/fruit.jpg",
+
+    desc: "Loaded with fruits",
+
+    category: "fruit"
+
+  },
+
+];
+ 
